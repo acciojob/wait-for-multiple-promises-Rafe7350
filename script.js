a18f8e0b1452cc020989e70cca71b2468c53578f
Rafe7350 <117887198+Rafe7350@users.noreply.github.com>
2023-03-14 23:34:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function byteSize(str) {
+  // Use the TextEncoder API to encode the string into bytes
+  const encoder = new TextEncoder();
+  const bytes = encoder.encode(str);
+  // Return the size of the bytes array
+  return bytes.length;
+}
+console.log(byteSize('hello world')); // 11
+console.log(byteSize('안녕하세요')); // 15
+console.log(byteSize('')); // 0
